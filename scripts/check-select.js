31a9268b6d4eb82bbdd1d3a0e57cc2d04b285682
annavarner <var21033@byui.edu>
2023-03-27 17:41:33 -0400

    sound is working!

--- scripts/check-select.js
+++ scripts/check-select.js
@@ -2,9 +2,8 @@ function showBgOptions() {
     if (document.getElementById("bg-checkbox").checked){
         let options = document.querySelector(".background-options");
         options.innerHTML = 
-        `
+        `<p> Choose Your Scene:</p>
         <select name="background" id="bg">
-            <option value="" selected disabled>Choose Your Scene</option>
             <option value="starry_sky">Starry Sky</option>
             <option value="rainbow_waterfall">Rainbow Waterfall</option>
             <option value="clouds">Clouds</option>
@@ -20,14 +19,13 @@ function showSoundOptions() {
     if (document.getElementById("sounds-checkbox").checked){
         let options = document.querySelector(".sound-options");
         options.innerHTML = 
-        `
+        `<p>Choose Your Sounds:</p>
         <select name="sounds" id="sounds">
-            <option value="" selected disabled>Choose Your Sound</option>
+            <option value="light-rain">Light Rain</option>
             <option value="brahams-lullaby">Brahm's Lullaby</option>
-            <option value="crickets">Crickets</option>
+            <option value="crickets">Crickets and Thunder</option>
             <option value="greensleeves">Greensleeves</option>
             <option value="heavy-rain">Heavy Rain</option>
-            <option value="light-rain">Light Rain</option>
             <option value="ocean-waves">Ocean Waves</option>
             <option value="relax">Relax</options>
             <option value="waves-on-sand-beach">Waves on Sand Beach</option>
@@ -39,13 +37,14 @@ function showSoundOptions() {
     };
 }
 function displayChill() {
-    display = document.querySelector(".selectDisplay")
-    display.classList.add("inactive")
-    let doneButton = document.querySelector("#done")
-    doneButton.classList.remove("inactive");
-    //const soundChoice = document.getElementById("sounds").value;
+    display = document.querySelector(".selectDisplay");
+    display.classList.add("inactive");
+    chillDisplay = document.querySelector(".chillDisplay");
+    chillDisplay.classList.remove("inactive");
 
-    // let chillPage = document.querySelector(".chillDisplay");
+    let doneButton = document.querySelector("#done");
+    doneButton.classList.remove("inactive");
+
 
     if (document.getElementById("bg-checkbox").checked) {
         let backgroundChoice = document.getElementById("bg").value;
@@ -53,22 +52,21 @@ function displayChill() {
         document.body.style.cssText+=`background-image:url(${image_url})`;
     }
     if (document.getElementById("affirm-checkbox").checked){
-        let affirmText = document.querySelector("#affirm");
+        let affirmText = document.querySelector(".affirm");
         affirmText.classList.remove("inactive");
-        const url = "https://www.affirmations.dev";
-        apiFetch(url);
-
+        apiFetch();
         }
     
     if (document.getElementById("breathe-checkbox").checked) {
-        let breatheSelect = document.querySelector("#breathe");
+        let breatheSelect = document.querySelector(".breathe");
         breatheSelect.classList.remove("inactive");
     }
     if (document.getElementById("sounds-checkbox").checked) {
-
-        chillPage.innerHTML = 
-        `<audio>
-        <source src="./sounds/${soundChoice}.mp3" type="audio/mpeg">
+        let soundChoice = document.getElementById("sounds").value;
+        let sounds = document.querySelector(".sound");
+        sounds.classList.remove("inactive");
+        sounds.innerHTML = `<audio controls autoplay loop>
+        <source src="./sounds/${soundChoice}.mp3" type="audio/mp3">
         Sorry! This browser doesn't support audio.
         </audio>`;
     }
@@ -77,9 +75,13 @@ function displayChill() {
 }
 
 
-async function apiFetch(url) {
+async function apiFetch() {
     try {
-        const response = await fetch(url);
+        const response = await fetch("https://www.affirmations.dev", {  
+            headers: {
+                Accept: "application/json"
+            }
+        });
         if (response.ok) {
             const affirmation = await response.json();
             affirmText.innerHTML = affirmation;
@@ -94,5 +96,6 @@ async function apiFetch(url) {
     }
 
 function endChill() {
-    location.reload()
+    window.location.reload();
+
 }
