deaa81c732de57d2c6d40bbd6a4cc6f2821a5d5b
annavarner <var21033@byui.edu>
2023-04-01 14:52:41 -0400

    added finish page

--- scripts/render-end-page.js
+++ scripts/render-end-page.js
@@ -26,20 +26,53 @@ function getTime() {
 
 }
 function displayResults(minutes, seconds) {
-    const chillTime = document.querySelector(".chill-time");
+
+    const chillTime = document.querySelector(".finish");
     chillTime.innerHTML = `    
     <figure>
         <img src="./images/penguin.webp" alt="an animated penguin" class="logo"></img>
     </figure>
     <div class="time-display">
-    Chill Time = ${minutes} minutes and ${seconds} seconds.
-    </div>`
-    feelBetter = chillTime.appendChild("div");
-    feelBetter.innerHTML = `<h2>Do you feel more calm?</h2>
-    <input type="radio" name="calmer" value="yes" id="morecalm">>
+    <p><strong>Chill Time = ${minutes} minutes and ${seconds} seconds.</strong></p>
+    <p><em>Do you feel more calm?</em></p>
+    <input type="radio" name="calmer" value="yes" onChange="calmer()">
     <label for="yes">yes!</label>
-    <input type="radio" name="calmer" value="no" id="notcalm">
-    <label for="no">no</label>`
+    <input type="radio" name="calmer" value="no" onChange="notCalmer()">
+    <label for="no">not really</label>
+    <span class="finish"></span>
+    </div>`
+}
+function calmer() {
+    finish = document.querySelector(".finish")
+    finish.innerHTML = `
+    <figure>
+        <img src="./images/penguin.webp" alt="an animated penguin" class="logo"></img>
+    </figure>
+    <p>I'm glad you're feeling calmer! Please come chill with me again the next time you want to feel more relaxed.</p>
+    <button id="end" onClick="reset()">Start Over</button>`
 
+}
 
+function notCalmer() {
+    finish = document.querySelector(".finish")
+    finish.innerHTML = `
+    <figure>
+        <img src="./images/penguin.webp" alt="an animated penguin" class="logo"></img>
+    </figure>
+    <p>Ok, you're not feeling calmer yet. We can try a different chill or here are some other things you can 
+        try to feel more relaxed:</p>
+    <ul>
+        <li>Go for a walk or run</li>
+        <li>Talk to a friend or family member</li>
+        <li>Listen to soft music</li>
+        <li>Take a warm bath</li>
+        <li>Make a list of things you're grateful for</li>
+        <li>Try some yoga poses</li>
+        <li>Draw or paint a picture</li>
+    </ul>
+    <button id="end" onClick="reset()">Try a New Chill</button>`
+
+}
+function reset() {
+    location.reload();
 }
